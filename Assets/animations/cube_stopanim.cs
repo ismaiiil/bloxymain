c3d0990eea2bd3cd81ceed3581a14aa68ea05106
mael <ismaiiil@live.com>
2017-12-13 16:11:17 +0400

    Revert "oops"
    This reverts commit c49383cf211fbde35d6831dd138f0592ed5b33be.

---
+++ Assets/animations/cube_stopanim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_stopanim : MonoBehaviour {
+	public Animator cube_enter;
+	// Use this for initialization
+	void Start () {
+		cube_enter = GetComponentInParent<Animator> ();
+	}
+
+	IEnumerator ExecuteAfterTime(float time){
+		yield return new WaitForSeconds(time);
+		cube_enter.enabled = false;
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "tile_normal") {
+			StartCoroutine (ExecuteAfterTime (2));
+		}
+		if (other.gameObject.tag == "deadzone") {
+			cube_enter.enabled = true;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

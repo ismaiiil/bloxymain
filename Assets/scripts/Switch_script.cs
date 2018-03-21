ad81c44354979bc3db7586e4aeb4b35cfbdefdfa
ismaiiil <ismaiiil@live.com>
2018-03-21 18:38:17 +0400

    bridge and switch part 1

---
+++ Assets/scripts/Switch_script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_script : MonoBehaviour {
+    public bool switched_pressed;
+    public float amount;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(((other.gameObject.tag == "cuboid_colliders")|| (other.gameObject.tag == "toporbot")) && switched_pressed == false)
+        {
+            switched_pressed = true;
+            transform.Translate(Vector3.down * amount, Space.World);
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+		
+	}
+}

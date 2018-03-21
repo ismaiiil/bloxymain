81a2505878ba851bcf31c3310410c7343bbb0471
ismaiiil <ismaiiil@live.com>
2018-03-21 20:18:28 +0400

    working bridge

--- Assets/scripts/Switch_script.cs
+++ Assets/scripts/Switch_script.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Switch_script : MonoBehaviour {
     public bool switched_pressed;
     public float amount;
+    public GameObject closebridges;
+    public GameObject counterswitches;
+
+     
 	// Use this for initialization
 	void Start () {
 		
@@ -12,14 +16,25 @@ public class Switch_script : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if(((other.gameObject.tag == "cuboid_colliders")|| (other.gameObject.tag == "toporbot")) && switched_pressed == false)
+        if((((other.gameObject.tag == "cuboid_colliders") &&(gameObject.tag == "round_switch"))|| (other.gameObject.tag == "toporbot")) && switched_pressed == false)
         {
             switched_pressed = true;
             transform.Translate(Vector3.down * amount, Space.World);
             gameObject.GetComponent<Renderer>().material.color = Color.green;
+            CloseBridges();
+
+
+
         }
     }
 
+    void CloseBridges() {
+        Animator[] animators = closebridges.GetComponentsInChildren<Animator>();
+        foreach (Animator animator in animators)
+        {
+            animator.SetBool("switch_closed", true);
+        }
+    }
     // Update is called once per frame
     void Update () {
 

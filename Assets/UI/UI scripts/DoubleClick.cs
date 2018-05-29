ef22ba33f029f3497a577bb7b626d1e2e84562f9
ismaiiil <ismaiiil@live.com>
2018-05-29 07:16:32 +0400

    some updates and fixes with random falls

---
+++ Assets/UI/UI scripts/DoubleClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DoubleClick : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    float lastClick = 0f;
+    float interval = 0.4f;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if ((lastClick + interval) > Time.time)
+        {
+            Debug.Log("single click");
+        }
+        else
+        {
+            lastClick = Time.time;
+            Debug.Log("double click");
+        }
+    }
+}
--- Assets/UI/UI scripts/LookAt.cs
+++ Assets/UI/UI scripts/LookAt.cs
@@ -17,14 +17,21 @@ public class LookAt : MonoBehaviour
 
     void Update()
     {
-        Vector3 pos = player.transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            Vector3 pos = player.transform.position;
 
-        float halfWidth = Screen.width * 0.5f;
-        float halfHeight = Screen.height * 0.5f;
-        float x = Mathf.Clamp((pos.x - halfWidth) / halfWidth, -1f, 1f);
-        float y = Mathf.Clamp((pos.y - halfHeight) / halfHeight, -1f, 1f);
-        mRot = Vector2.Lerp(mRot, new Vector2(x, y), Time.deltaTime * 5f);
+            float halfWidth = Screen.width * 0.5f;
+            float halfHeight = Screen.height * 0.5f;
+            float x = Mathf.Clamp((pos.x - halfWidth) / halfWidth, -1f, 1f);
+            float y = Mathf.Clamp((pos.y - halfHeight) / halfHeight, -1f, 1f);
+            mRot = Vector2.Lerp(mRot, new Vector2(x, y), Time.deltaTime * 5f);
 
-        mTrans.localRotation = mStart * Quaternion.Euler(-mRot.x * player.transform.position.x, -mRot.y * player.transform.position.z, 0f);
+            mTrans.localRotation = mStart * Quaternion.Euler(-mRot.x * player.transform.position.x, -mRot.y * player.transform.position.z, 0f);
+        }
+
+
+        
     }
 }
